4edd67bd435dcbaf87250e52b2bf1fa4c41054a9
Magali <magiiee@live.com.ar>
2022-06-18 15:45:08 -0300

    ADD Delete

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
-import { CheckIcon } from '@heroicons/react/solid';
+import { CheckIcon, XCircleIcon } from '@heroicons/react/solid';
 import Modal from '@common/Modal';
 import FormProduct from '@components/FormProduct';
 import axios from 'axios';
 import endPoints from '@services/api';
 import useAlert from '@hooks/useAlert';
 import Alert from '@common/Alert';
+import { deleteProduct } from '@services/api/products';
 
 export default function Products() {
   const [open, setOpen] = useState(false);
@@ -23,9 +24,20 @@ export default function Products() {
     }
   }, [alert]);
 
+  const handleDelete = (id) => {
+    deleteProduct(id).then(() =>{
+      setAlert({
+        active: true,
+        message: 'Delete product successfully' ,
+        type: 'error',
+        autoClose: true,
+      });
+    });
+  };
+
   return (
     <>
-      <Alert alert={alert} handleClose={toggleAlert}  />
+      <Alert alert={alert} handleClose={toggleAlert} />
       <div className="lg:flex lg:items-center lg:justify-between mb-8 ">
         <div className="flex-1 min-w-0">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">List of Products</h2>
@@ -96,9 +108,7 @@ export default function Products() {
                         </a>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
-                          Delete
-                        </a>
+                        <XCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-400 cursor-pointer" aria-hidden="true" onClick={() => handleDelete(product.id)} />
                       </td>
                     </tr>
                   ))}
@@ -109,7 +119,7 @@ export default function Products() {
         </div>
       </div>
       <Modal open={open} setOpen={setOpen}>
-        <FormProduct setOpen={setOpen} setAlert={setAlert}  />
+        <FormProduct setOpen={setOpen} setAlert={setAlert} />
       </Modal>
     </>
   );
