6ce09f4a90d60c0dce32822e4c86b80215f372f9
Magali <magiiee@live.com.ar>
2022-06-18 17:08:53 -0300

    ADD Edit y logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -35,8 +35,17 @@ function useProvideAuth() {
       setUser(user);
     }
   };
+
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     signIn,
+    logout,
   };
 }
